02ecef575ce0850ddcd91a823dac95aaae3b069f
sandersanches <sandersanches@gmail.com>
2024-07-01 21:36:45 -0300

    Aula Testando Messaging

---
+++ src/services/messaging.spec.ts
@@ -0,0 +1,29 @@
+import { Messaging } from './messaging';
+
+const createSut = () => {
+  return new Messaging();
+};
+
+describe('Messaging', () => {
+  afterEach(() => jest.clearAllMocks()); //Limpar os mocks após cada teste
+
+  it('should retturn undefined', () => {
+    // System under test -> é a classe principal que está sendo testada
+    const sut = createSut();
+    expect(sut.sendMessage('teste')).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with ""Mensagem enviada:", "teste""', () => {
+    const sut = createSut();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.sendMessage('teste');
+    expect(consoleSpy).toHaveBeenCalledWith('Mensagem enviada:', 'teste');
+  });
+});
